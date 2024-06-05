c934dac356d1bb0a1d2bf59fca4f12d68cd68479
Ewerton Henrique <ewerton.ramiro@gmail.com>
2024-06-05 12:52:15 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dine Turismo</title>
+    <link rel="stylesheet" href="estilo.css" />
+</head>
+
+<!-- Navbar do projeto. -->
+<body>
+    <header>
+        <section class="sect"></section>
+        <nav>
+            <a class="logo" href="/">DINE TURISMO</a>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="#">Viagens</a></li>
+                <li><a href="#">Hoteis</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+
+        <!-- Colocando o java do projeto. -->
+        <script src="navbar.js"></script>
+</body>
+
+</html>
